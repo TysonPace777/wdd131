b5325185bed534b7b3d5557893664d26b7cc19bc
tysonpace777 <32.tpace@gmail.com>
2025-01-09 15:48:22 -0700

    javascript fixed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,7 @@
 const today = new Date();
-const currentyear = document.querySelector("#year")
 
-currentyear.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+const currentyear = document.querySelector("#currentyear")
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 const lastmodified = document.querySelector("#lastmodified");
-lastmodified.innerHTML = `Last Modified: <span class="highlight">${document.lastmodified}</span>`;
+lastmodified.innerHTML = `Last Modified: <span class="highlight">${document.lastModified}</span>`;
