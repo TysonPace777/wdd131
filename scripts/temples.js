b77f37167085ef9265618c80123e9a40b3710899
tysonpace777 <32.tpace@gmail.com>
2025-01-15 13:45:44 -0700

    saved

--- scripts/temples.js
+++ scripts/temples.js
@@ -14,6 +14,6 @@ const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
 });
