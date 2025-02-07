898fc9a7b0b6be9991fbbf104162e5672f77c161
tysonpace777 <32.tpace@gmail.com>
2025-02-06 17:37:28 -0700

    js created and fixed

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button');
+const button = document.getElementById('button');
 
 const today = new Date();
 
@@ -9,44 +9,46 @@ const lastmodified = document.querySelector("#lastmodified");
 lastmodified.innerHTML = `Last Modified: <span class="highlight">${document.lastModified}</span>`;
 
 const products = [
-    {
-      id: "fc-1888",
-      name: "flux capacitor",
-      averagerating: 4.5
-    },
-    {
-      id: "fc-2050",
-      name: "power laces",
-      averagerating: 4.7
-    },
-    {
-      id: "fs-1987",
-      name: "time circuits",
-      averagerating: 3.5
-    },
-    {
-      id: "ac-2000",
-      name: "low voltage reactor",
-      averagerating: 3.9
-    },
-    {
-      id: "jj-1969",
-      name: "warp equalizer",
-      averagerating: 5.0
-    }
-  ];
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
 
 const sct = document.getElementById('sct')
 
 products.forEach(product => {
   const option = document.createElement('option');
   option.textContent = product.name;
-  option.value = products.id
+  option.value = product.id
   sct.appendChild(option);
 })
 
 function submitNumber() {
-  localStorage.setItem('Reviews Submitted', JSON.stringify++);
+  let currentCount = JSON.parse(localStorage.getItem('Reviews Submitted')) || 0;
+  currentCount++;
+  localStorage.setItem('Reviews Submitted', JSON.stringify(currentCount));
 }
 
 button.addEventListener('click', () => {
