dc9b2a7e03423a89192c543be94bc85b7a738afb
tysonpace777 <32.tpace@gmail.com>
2025-02-12 14:49:46 -0700

    all three sites made and most of css finished along with webp images and content

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,3 +1,4 @@
+//Date
 const today = new Date();
 
 const currentyear = document.querySelector("#currentyear")
@@ -5,3 +6,19 @@ currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 const lastmodified = document.querySelector("#lastmodified");
 lastmodified.innerHTML = `Last Modified: <span class="highlight">${document.lastModified}</span>`;
+
+//Button
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const body = document.body;
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+
+    if (!hamButton.classList.toggle('open')) {
+        body.style.display = 'grid';
+    }
+});
