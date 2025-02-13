1507e6aab8d204d40afe19aa1b1831ecefe77977
tysonpace777 <32.tpace@gmail.com>
2025-02-13 12:43:28 -0700

    form styled

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -22,3 +22,48 @@ hamButton.addEventListener('click', () => {
         body.style.display = 'grid';
     }
 });
+
+//Submit Button
+const button = document.getElementById('button')
+
+const games = [
+    {
+      id: "11111",
+      name: "Action",
+    },
+    {
+      id: "22222",
+      name: "Horror",
+    },
+    {
+      id: "33333",
+      name: "Survival",
+    },
+    {
+      id: "44444",
+      name: "Fantasy",
+    },
+    {
+      id: "55555",
+      name: "Other",
+    }
+  ];
+  
+  const sct = document.getElementById('sct')
+  
+  games.forEach(game => {
+    const option = document.createElement('option');
+    option.textContent = game.name;
+    option.value = game.id
+    sct.appendChild(option);
+  })
+  
+  function submitNumber() {
+    let currentCount = JSON.parse(localStorage.getItem('Games Submitted')) || 0;
+    currentCount++;
+    localStorage.setItem('Reviews Submitted', JSON.stringify(currentCount));
+  }
+  
+  button.addEventListener('click', () => {
+    submitNumber()
+  })
